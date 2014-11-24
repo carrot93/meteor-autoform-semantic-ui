7631bb25aa5854ac73e21afd31b7f4d3a971c8b4
James Lefrère <jlefrere@gmail.com>
2014-11-24 21:02:06 +0100

    Bumped version, added dependency on my semantic-ui package

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "jameslefrere:autoform-semantic-ui",
   summary: "Semantic UI theme for Autoform",
-  version: "0.1.0",
+  version: "0.1.1",
   git: "https://github.com/JamesLefrere/meteor-autoform-semantic-ui.git"
 });
 
@@ -9,6 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom("1.0");
   api.use(["coffeescript", "templating", "underscore"], "client");
   api.use("aldeed:autoform@4.0.7");
+  api.use("jameslefrere:semantic-ui@0.1.1");
   api.addFiles([
     "semanticUI.html",
     "semanticUI.css",
