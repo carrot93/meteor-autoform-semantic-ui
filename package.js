00ea0e12376f38436c8b3c4d586c35162b7bea6d
James Lefrère <jlefrere@gmail.com>
2014-11-24 17:36:45 +0100

    Bumped autoform version

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom("1.0");
   api.use(["coffeescript", "templating", "underscore"], "client");
-  api.use("aldeed:autoform@4.0.0");
+  api.use("aldeed:autoform@4.0.7");
   api.addFiles([
     "semanticUI.html",
     "semanticUI.css",
