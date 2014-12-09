6104c8701952002e708cceeb77ac12f051860443
James Lefrère <jlefrere@gmail.com>
2014-12-09 20:37:52 +0100

    Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "jameslefrere:autoform-semantic-ui",
   summary: "Semantic UI theme for Autoform",
-  version: "0.1.6",
+  version: "0.1.7",
   git: "https://github.com/JamesLefrere/meteor-autoform-semantic-ui.git"
 });
 
